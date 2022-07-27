51dabeec9ffb7b37bdab9f5cfda27868b5aec506
AlejandroV2042419 <alejandro.villamil@correounivalle.edu.co>
2022-07-26 22:14:32 -0500

    Creacion de api para crear Trabajador

--- backend/src/index.js
+++ backend/src/index.js
@@ -1,6 +1,8 @@
 const express = require("express");
 const  auth  = require('./routes/auth')
 const  client  = require('./routes/client')
+const  worker  = require('./routes/worker')
+
 
 const app = express();
 const PORT = process.env.PORT || 4001;
@@ -10,6 +12,8 @@ app.use(express.json());
 app.use("/api/auth/", auth);
 
 app.use("/api/client/", client);
+
+app.use("/api/worker/", worker);
 
 app.listen(PORT, () => {
   console.log(`🚀 Servidor iniciado el puerto ${PORT}`);
--- backend/src/routes/workoutRoutes.js
+++
@@ -1,7 +0,0 @@
-const express = require("express");
-const router = express.Router();
-const workoutController = require("../controllers/workoutController");
-
-router.get("/", workoutController.getAllWorkouts)
-
-module.exports = router;
