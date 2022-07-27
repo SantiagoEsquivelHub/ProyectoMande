eac03cb1bf2fcccca8a925fd1a3d985b2b786d09
AlejandroV2042419 <alejandro.villamil@correounivalle.edu.co>
2022-07-26 20:42:14 -0500

    Acutalizar creacion de cliete con todos los campos

--- backend/src/services/client.js
+++ backend/src/services/client.js
@@ -2,13 +2,14 @@ const pool = require("../database/db_pool_connect");
 const bcrypt = require('bcryptjs');
 
 const insertClient = async (data) => {
-    const { email, name, password } = data;
+    const password = await data.contraseña_cliente;
+    const { email_cliente, nombre_cliente , numero_celular_cliente, url_recibo_publico, rol_cliente, direccion_residencia_cliente} = data;
     const passwordHash = await bcrypt.hash(password, 8);
 
-    const cliente = await pool.query(`SELECT * FROM cliente WHERE email_cliente = '${email}';`);
+    const cliente = await pool.query(`SELECT * FROM cliente WHERE email_cliente = '${email_cliente}';`);
 
     if (cliente.rows == '') {
-        const createClientQuery = await pool.query(`INSERT INTO cliente(email_cliente ,nombre_cliente, contraseña_cliente) VALUES('${email}' , '${name}' , '${passwordHash}');`);
+        const createClientQuery = await pool.query(`INSERT INTO cliente(email_cliente ,nombre_cliente, contraseña_cliente, numero_celular_cliente, url_recibo_publico, rol_cliente, direccion_residencia_cliente) VALUES('${email_cliente}' , '${nombre_cliente}' , '${passwordHash}', ${numero_celular_cliente}, '${url_recibo_publico}', '${rol_cliente}', '(${direccion_residencia_cliente})' );`);
 
         if (createClientQuery != '') {
             return true;
