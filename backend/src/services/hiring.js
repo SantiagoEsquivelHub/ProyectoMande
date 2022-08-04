9e88ef1af16318174066d4652a0049b1ae52a730
Santiago Sánchez Esquivel <santiagosancheze8.1@gmail.com>
2022-08-04 14:00:51 -0500

    Correcion en endpoint para insertar descripcion en la contratacion

--- backend/src/services/hiring.js
+++ backend/src/services/hiring.js
@@ -2,13 +2,13 @@ const pool = require("../database/db_pool_connect");
 const { get } = require("../routes/hiring");
 
 const hireWorker = async (data) => {
-    const { id_cliente, id_trabajador, id_labor, id_estado_contratacion = 1, calificacion_contratacion = null } = data;
+    const { id_cliente, id_trabajador, id_labor, descripcion, id_estado_contratacion = 1, calificacion_contratacion = null } = data;
 
     const searchIdWorker = await pool.query(`select id_labor_trabajador from labor_trabajador where id_trabajador = ${id_trabajador} and id_labor = ${id_labor}`)
 
     let id_labor_trabajador = searchIdWorker.rows[0].id_labor_trabajador;
-
-    const createHiring = await pool.query(`INSERT INTO contratacion(id_cliente, id_trabajador, id_labor_trabajador, id_estado_contratacion , calificacion_contratacion) VALUES(${id_cliente} , ${id_trabajador} , ${id_labor_trabajador} , ${id_estado_contratacion}, ${calificacion_contratacion}));`);
+    console.log("descripcion", descripcion)
+    const createHiring = await pool.query(`INSERT INTO contratacion(id_cliente, id_trabajador, id_labor_trabajador, id_estado_contratacion , calificacion_contratacion, descripcion) VALUES(${id_cliente} , ${id_trabajador} , ${id_labor_trabajador} , ${id_estado_contratacion}, ${calificacion_contratacion}, '${descripcion}');`);
     const actualizarEstadoTrabajador = await pool.query(`UPDATE trabajador
      SET id_estado = 2
      WHERE id_trabajador = ${id_trabajador};`);
@@ -65,7 +65,7 @@ const viewHiring = async (data) => {
 
 const getHiringFinished = async (data) => {
     const { id, tipo } = data;
-    
+
     if (tipo == "id_cliente") {
         let get = await pool.query(`SELECT t.nombre_trabajador, l.nombre_labor , c.pago, to_char(c.fecha_pago, 'yy/mm/dd') as fecha_pago, c.calificacion_contratacion
         FROM contratacion AS c
